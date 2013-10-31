4f75ecc230530da280499a0e75cf515f63532f72
Danny Aldridge <dannyaldridge@gmail.com>
2013-10-31 14:19:12 +0000

    user features steps

--- features/step_definitions/users_steps.rb
+++ features/step_definitions/users_steps.rb
@@ -1,5 +1,5 @@
 Given (/^I go to the sign up page$/) do
-  visit path_to(users_new)
+  visit path_to(new)
 end
 
 When (/^I fill in "(.*?)" for "(.*?)"$/) do |james, login|
@@ -23,3 +23,35 @@ end
 
 Then(/^I should be redirected to the "(.*?)" page$/) do |teams|
 end
+
+# Given (/^I go to the log in page$/) do
+#   # visit path_to(login)
+# end
+
+# When (/^I fill in "(.*?)" for "(.*?)"$/) do |james, login|
+#   # fill_in(name, :with => James)
+# end
+
+# When (/^I am already a created user$/) do
+
+# end
+
+# When (/^I click "(.*?)"$/) do |login|
+#   # click_button(log_in)
+# end
+
+# Then (/^I should see "(.*?)"$/) do |thanks|
+# end
+
+# Then(/^I should be redirected to the "(.*?)" page$/) do |teams|
+# end
+
+# Given (/^I am logged in as "(.*?)"$/) do |arg|
+# end
+
+# When (/^I click "(.*?)"$/) do |logout|
+#   # click_button(log_out)
+# end
+
+# Then(/^I should be logged out and redirected to the "(.*?)"page$/) do |logout|
+# end
