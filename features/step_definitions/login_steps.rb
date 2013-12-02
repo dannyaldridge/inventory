e0f26821e9699efd71a433565f525babf4f5be3e
Adam Dawkins <adamdawkins@gmail.com>
2013-12-02 23:55:49 +0000

    [#61728714 #61412998] Add step definitions for logout scenario

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,5 +1,9 @@
 require_relative '../support/env'
 
-Given (/^I am logged in as "(.*?)"$/) do |james|
-	
+Given (/^I am logged in as "James"$/) do 
+  User.create(name: "James", password: "123456", password_confirmation: "123456")
+  visit "/sessions/new"
+  fill_in "Name", with: "James"
+  fill_in "Password", with: "123456"
+  click_button "Log in"
 end
