22da3afe9a81cccde5eb957adca1386a3fb808c4
Danny Aldridge <dannyaldridge@gmail.com>
2013-12-04 18:34:39 +0000

    [#61718846] Update tests for ununique team name

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -2,12 +2,34 @@ require 'spec_helper'
 
 describe TeamsController do
 
-	describe "POST create" do
-		context "With a unique team name" do
-			it "Creates a new team in the database" do
-				expect to change(Team, :count).by 1
+	describe "create" do
+		context "with a unique name" do
+			it "creates a new team in the database" do
+				expect {post 'create', team: FactoryGirl.attributes_for(:team)}.to change(Team, :count).by 1
+			end
+
+			it "sets the flash message to 'Team added'" do
+				post 'create', team: FactoryGirl.attributes_for(:team)
+				expect(flash[:notice]).to eq "Team added"
+			end
+
+			it "redrects to the show teams page" do
+				post 'create', team: FactoryGirl.attributes_for(:team)
+				expect(response).to redirect_to ('/teams/1')
 			end
 		end
-	end
 
+		context "With a non unique name" do
+			before :each do
+				@team = FactoryGirl.create(:team)
+        post 'create',
+          team: {name: @team.name}
+        end
+
+			it "redrects to the new team page" do
+				response.should render_template('teams/new')
+			end
+
+		end
+	end
 end
