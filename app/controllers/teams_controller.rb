1297104ab7753e83b248fbff9c1e4b72eb534cf6
Danny Aldridge <dannyaldridge@gmail.com>
2013-12-04 18:29:51 +0000

    [#61718846]Apply team validation

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,10 +6,28 @@ class TeamsController < ApplicationController
 
   def create
   	@team = Team.new team_params
-  	
+
+  	if @team.save
+  		flash[:notice] = "Team added"
+  		redirect_to @team
+  	else
+  		render 'new'
+  	end
   end
 
+  # def index
+  #   @team = Team.all
+  # end
 
+  def show
+    @team = Team.find params[:id]
+  end
+
+  private 
+
+  def team_params
+    params.require(:team).permit(:name)
+  end
 
 
 end
--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -2,4 +2,12 @@ require 'spec_helper'
 
 describe TeamsController do
 
+	describe "POST create" do
+		context "With a unique team name" do
+			it "Creates a new team in the database" do
+				expect to change(Team, :count).by 1
+			end
+		end
+	end
+
 end
