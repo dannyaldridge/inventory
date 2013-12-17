215a9924b2cfaa28a03ecc9d0868952a53bc134f
Adam Dawkins <adamdawkins@gmail.com>
2013-12-17 00:51:24 +0000

    [#61767208] Restrict user addition to logged in users

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,8 +2,23 @@ require 'spec_helper'
 
 describe UsersController do
 
+  describe "When Logged out" do
+    include ControllerSpecHelper
+
+    before(:each) do
+      session[:user_id] = nil
+      @login_notice = "you must be logged in to view that page"
+    end
+
+    it "does not let the user use the action" do
+      controller_actions_should_fail_if_not_logged_in(:users, except: [:show])
+    end
+  end
 
   describe "POST create" do
+    before(:each) do
+      stub_login
+    end
     context "with valid user attributes" do
       it "creates a new user in the database" do
         expect {
