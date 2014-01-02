083f3cb5b1d205161299b3878fdaaa88ad3ef78c
Danny Aldridge <dannyaldridge@gmail.com>
2014-01-02 16:22:23 +0000

    [#61721210] Write spec for destroy method

--- spec/controllers/itemtypes_controller_spec.rb
+++ spec/controllers/itemtypes_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require 'controller_spec_helper'
 
-describe TeamsController do
+describe ItemtypesController do
   
   describe "#new" do
       before :each do
@@ -9,14 +9,32 @@ describe TeamsController do
         get 'new'
       end
 
-    	it "Creates a new team object" do
-      	expect(assigns[:team]).to be_instance_of Team
+    	it "Creates a new item type object" do
+      	expect(assigns[:itemtype]).to be_instance_of Itemtype
     	end
 
-    	it "Creates the new team page" do
-      	response.should render_template('teams/new')
+    	it "Creates the new item type page" do
+      	response.should render_template('itemtypes/new')
     	end
   	end
+
+  describe "#destroy" do
+    before(:each) do 
+      stub_login
+      @itemtype = FactoryGirl.create(:itemtype)
+
+      delete "destroy", id: @itemtype.id
+    end
+    
+    it "removes the item type from the database" do
+      expect(Itemtype.exists?(@itemtype.id)).to be_nil
+    end
+
+    it "redirects to the index action" do
+        expect(response).to redirect_to itemtypes_path
+    end
+
+  end
 
   describe "#create" do
       before :each do
@@ -25,30 +43,30 @@ describe TeamsController do
       end
 
       context "with a unique name" do
-      it "creates a new team in the database" do
-        expect {post 'create', team: FactoryGirl.attributes_for(:team)}.to change(Team, :count).by 1
+      it "creates a new item type in the database" do
+        expect {post 'create', itemtype: FactoryGirl.attributes_for(:itemtype)}.to change(Itemtype, :count).by 1
       end
 
-      it "sets the flash message to 'Team added'" do
-        post 'create', team: FactoryGirl.attributes_for(:team)
-        expect(flash[:notice]).to eq "Team added"
+      it "sets the flash message to 'Item type added'" do
+        post 'create', itemtype: FactoryGirl.attributes_for(:itemtype)
+        expect(flash[:notice]).to eq "Item Type added"
       end
 
-      it "redrects to the show teams page" do
-        post 'create', team: FactoryGirl.attributes_for(:team)
-        expect(response).to redirect_to ('/teams/1')
+      it "redrects to the show item type page" do
+        post 'create', itemtype: FactoryGirl.attributes_for(:itemtype)
+        expect(response).to redirect_to ('/itemtypes/1')
       end
     end
 
     context "With a non unique name" do
       before :each do
-        @team = FactoryGirl.create(:team)
+        @itemtype = FactoryGirl.create(:itemtype)
         post 'create',
-          team: {name: @team.name}
+          itemtype: {name: @itemtype.name}
         end
 
-      it "redrects to the new team page" do
-        response.should render_template('teams/new')
+      it "redrects to the new item type page" do
+        response.should render_template('itemtypes/new')
       end
 
     end
