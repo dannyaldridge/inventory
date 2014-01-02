3bf7e857bc39a7f7d6ba75a58a7b02d3bf9ce16a
Danny Aldridge <dannyaldridge@gmail.com>
2014-01-02 15:20:54 +0000

    [#61721210] Write tests for create action

--- spec/controllers/itemtypes_controller_spec.rb
+++ spec/controllers/itemtypes_controller_spec.rb
@@ -18,7 +18,40 @@ describe TeamsController do
     	end
   	end
 
-  descibe "#create" do
+  describe "#create" do
+      before :each do
+        stub_login
+        get 'new'
+      end
+
+      context "with a unique name" do
+      it "creates a new team in the database" do
+        expect {post 'create', team: FactoryGirl.attributes_for(:team)}.to change(Team, :count).by 1
+      end
+
+      it "sets the flash message to 'Team added'" do
+        post 'create', team: FactoryGirl.attributes_for(:team)
+        expect(flash[:notice]).to eq "Team added"
+      end
+
+      it "redrects to the show teams page" do
+        post 'create', team: FactoryGirl.attributes_for(:team)
+        expect(response).to redirect_to ('/teams/1')
+      end
+    end
+
+    context "With a non unique name" do
+      before :each do
+        @team = FactoryGirl.create(:team)
+        post 'create',
+          team: {name: @team.name}
+        end
+
+      it "redrects to the new team page" do
+        response.should render_template('teams/new')
+      end
+
+    end
   end
 
 end
