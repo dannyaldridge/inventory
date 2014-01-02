a9ed45dd2b83fc9d43453bdcf3f212dda78375ec
Danny Aldridge <dannyaldridge@gmail.com>
2014-01-02 14:34:02 +0000

    [#61721210] Define create method

--- app/controllers/itemtypes_controller.rb
+++ app/controllers/itemtypes_controller.rb
@@ -4,4 +4,21 @@ def new
 	@itemtype = Itemtype.new
 end
 
+def create
+	@itemtype = Itemtype.new itemtype_params
+	
+	if @itemtype.save
+  	flash[:notice] = "Item Type added"
+  	redirect_to @itemtype
+  else
+  	render 'new'
+  end 
+end
+
+private 
+
+  def itemtype_params
+    params.require(:itemtype).permit(:name)
+  end
+
 end
