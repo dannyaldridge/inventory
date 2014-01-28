ebb8bd75ccceed86c9d1eee945533b648dc842da
Danny Aldridge <dannyaldridge@gmail.com>
2014-01-28 17:43:57 +0000

    Remove unwanted files

--- db/schema.rb
+++ db/schema.rb
@@ -27,17 +27,11 @@ ActiveRecord::Schema.define(version: 20140114204641) do
   end
 
   create_table "itemtypes", force: true do |t|
-  end
-
-  create_table "kits", force: true do |t|
     t.string   "name"
-    t.string   "type"
-    t.integer  "quantity"
-    t.string   "description"
-    t.integer  "squad"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
 
   create_table "users", force: true do |t|
     t.string   "name"
